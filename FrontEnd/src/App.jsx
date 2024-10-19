637d0ed32ee7082ef8a9c8bdbc83f72476bfc3a3
Danny040 <dann0051@gmail.com>
2024-10-19 10:56:13 +0300

    Client side is ready

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import style from "./App.module.css";
+import sqlLogo from "./assets/sql_logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [queryDescription, setQueryDescription] = useState("");
+  const submitFun = (event) => {
+    event.preventDefault();
+    console.log("Form has been submited: ", queryDescription);
+  };
+  // useEffect(() => {
+  //   console.log(queryDescription);
+  // }, [queryDescription]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={style.main}>
+      <img src={sqlLogo} className={style.icon} />
+      <h3>SQL Query Generator</h3>
+      <h5>Powered by AI</h5>
+      <form onSubmit={submitFun}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query..."
+          onChange={(event) => setQueryDescription(event.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
